487f47fa3b5dc1b2d64b7689a46d1187f2f2f975
kevindoveton <kevindoveton344@gmail.com>
2016-07-30 13:58:48 +0930

    fixed up xml

--- www/js/factories/readingService.js
+++ www/js/factories/readingService.js
@@ -34,37 +34,37 @@ angular.module('starter').factory('dataService', function($http, $localstorage)
 	
 })
 
-angular.module('starter').factory('weatherService', function($http, $localstorage)
-{
-	//TODO: Cache - just for this session.
+// angular.module('starter').factory('weatherService', function($http, $localstorage)
+// {
+// 	//TODO: Cache - just for this session.
 	
-	var dataService = {
+// 	var dataService = {
 		
-		async: function() {
-			var items = [];
-			var promise = $.get('http://clients.kdoveton.com/govhack/?q=ftp://ftp.bom.gov.au/anon/gen/fwo/IDS10037.xml', function(data) {
-			}).then(function (response)
-			{
-				var $xml = $(response);
-				$xml.find("item").each(function() {
-					var $this = $(this),
-			        	item = {
-							title: $this.find("image").find("title").text(),
-							description: $this.find("description").text(),
-							link: $this.find("link").text(),
-							date: $this.find("date").text(),
-							imageUrl: $this.find("image").find("url").text(),
-						}
-					items.push(item);
-				});
-				return items;
-			});
+// 		async: function() {
+// 			var items = [];
+// 			var promise = $.get('http://clients.kdoveton.com/govhack/?q=ftp://ftp.bom.gov.au/anon/gen/fwo/IDS10037.xml', function(data) {
+// 			}).then(function (response)
+// 			{
+// 				var $xml = $(response);
+// 				$xml.find("item").each(function() {
+// 					var $this = $(this),
+// 			        	item = {
+// 							title: $this.find("image").find("title").text(),
+// 							description: $this.find("description").text(),
+// 							link: $this.find("link").text(),
+// 							date: $this.find("date").text(),
+// 							imageUrl: $this.find("image").find("url").text(),
+// 						}
+// 					items.push(item);
+// 				});
+// 				return items;
+// 			});
 			
-			// console.log(promise);
-			return promise;
-			}
-		};
+// 			// console.log(promise);
+// 			return promise;
+// 			}
+// 		};
 
-	return dataService;
+// 	return dataService;
 	
-})
+// })
