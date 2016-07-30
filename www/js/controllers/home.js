91296e9fa728b22ae7f6d29cee856e6f31ed2b71
kevindoveton <kevindoveton344@gmail.com>
2016-07-30 17:21:35 +0930

    weather attempt

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -1,23 +1,28 @@
 /*===========   Home    ===========*/
 // TODO:
 // Load Background Image
-angular.module('starter.controllers').controller('HomeCtrl', function($ionicPlatform, $http, $scope, $rootScope, $state, dataService, $localstorage, $window) { 
+angular.module('starter.controllers').controller('HomeCtrl', function($ionicPlatform, $http, $scope, $rootScope, $state, dataService, $localstorage, $window, weatherService) { 
 	dataService.async().then(function(d) {
 		$scope.data = d;
 		$scope.apply;
 	});
 
+	weatherService.async().then(function(d) {
+		// $scope.data = d;
+		// $scope.apply;
+	});
+
 	
 
-	var promise = $http.get('http://clients.kdoveton.com/govhack?q=http://www.adelaidecitycouncil.com/whats-on/event/alpine-winter-village-1', function(data) {
-			}).then(function (response)
-			{
-				var $html = $($.parseHTML(response.data));
-				// console.log()
-				$html.find("section.venue").each(function() {
-					var $this = $(this);
-					console.log($this.find("span.val").text());
-				});
-			});
+	// var promise = $http.get('http://clients.kdoveton.com/govhack?q=http://www.adelaidecitycouncil.com/whats-on/event/alpine-winter-village-1', function(data) {
+	// 		}).then(function (response)
+	// 		{
+	// 			var $html = $($.parseHTML(response.data));
+	// 			// console.log()
+	// 			$html.find("section.venue").each(function() {
+	// 				var $this = $(this);
+	// 				console.log($this.find("span.val").text());
+	// 			});
+	// 		});
 
 })
